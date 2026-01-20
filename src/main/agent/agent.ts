b0d01f1e25c4a41cdd6fa05a0dbfe561624060c8
justine <justinemangaliman@gmail.com>
2026-01-19 21:38:36 -0700

    tools for replay info and replay gameplay data
    replay info being the replay/beatmap hashes, online score id, combos,
    and number of 300s and such. replay gameplay data means the user
    presses, cursor locations, and timestamps.

--- src/main/agent/agent.ts
+++ src/main/agent/agent.ts
@@ -1,49 +1,69 @@
-import * as z from 'zod';
 import 'dotenv/config';
 import { createAgent, tool, type ToolRuntime } from 'langchain';
+import { parseOsrFile } from '../../utils/replay-parser/parser';
 
 const systemPrompt = `You are an osu! assistant who helps players find flaws in their replays. You analyze their strengths and weaknesses, their cursor movement, tapping style, what could have made them miss, and so on.
 
 You have access to two tools:
 
-- get_replay_data: use this to get the replay data of a .osr file.
-- analyze_replay: use this to analyze the replay data. you must call get_replay_data first before calling this tool.
-- analyze_replay_at_timestamp: use this to analyze the replay data at a certain timestamp, with your choice of how big the window is. you must call get_replay_data first before calling this tool.
-- suggest_beatmaps_weaknesses: use this to recommend beatmaps to the player that address their weaknesses.
-- suggest_beatmaps_strengths: use this to recommend beatmaps to the player that focus on their strengths.
-- compare_with_past_replays: use this to
+- get_replay_path: use this to get the replay path of a .osr file.
+- get_replay_info: use this to get the replay info of a .osr file. it gives you the beatmap MD5 hash, the replay MD5 hash, game mode, game version, max combo, used mods, number of 50s/100s/300s, the online score id, if it was a perfect combo or not, and the total score.
+- get_replay_gameplay_data: use this to get the replay gameplay data of a .osr file. it includes a replay timestamp, the x and y coordinate of the cursor at the timestamp, and the key presses at the timestamp.
 
-If a user asks you for the weather, make sure you know the location. If you can tell from the question that they mean wherever they are, use the get_user_location tool to find their location.`;
+You must get the replay path of a .osr file before calling get_replay_info or get_replay_gameplay_data.
+`;
 
-const getWeather = tool(({ city }) => `It's always sunny in ${city}!`, {
-  name: 'get_weather',
-  description: 'Get the weather for a given city',
-  schema: z.object({
-    city: z.string().describe('The city to get the weather for.'),
-  }),
-});
+type AgentRuntime = ToolRuntime<unknown, { replay_path: string }>;
 
-type AgentRuntime = ToolRuntime<unknown, { user_id: string }>;
-
-const getUserLocation = tool(
-  (_, config: AgentRuntime) => {
-    const { user_id } = config.context;
-    return user_id === '1' ? 'Florida' : 'SF';
-  },
-  {
-    name: 'get_user_location',
-    description: 'Retrieve user information based on user ID',
+const getReplayPath = tool(
+  async (_, config: AgentRuntime) => {
+    const replayPath = getReplayPath();
+    return replayPath;
   }
 );
 
-const responseFormat = z.object({
-  punny_response: z.string(),
-  weather_conditions: z.string().optional(),
-});
+const getReplayInfo = tool(
+  async (_, config: AgentRuntime) => {
+    const { replay_path } = config.context;
+    const replayInfo = await parseOsrFile(replay_path);
+    const output = {
+      "replay_md5_hash": replayInfo.replayMD5,
+      "beatmap_md5_hash": replayInfo.beatmapMD5,
+      "online_score_id": replayInfo.onlineScoreId,
+      "game_mode": replayInfo.beatmapMD5,
+      "game_version": replayInfo.beatmapMD5,
+      "number_of_300s": replayInfo.numberOf300s,
+      "number_of_100s": replayInfo.numberOf100s,
+      "number_of_50s": replayInfo.numberOf50s,
+      "max_combo": replayInfo.maxCombo,
+      "used_mods": replayInfo.mods,
+      "perfect_combo": replayInfo.perfectCombo,
+      "total_score": replayInfo.totalScore
+    };
+
+    return JSON.stringify(output, null, 2);
+  },
+  {
+    name: 'get_replay_info',
+    description: 'Retrieve replay information',
+  }
+);
+
+const getReplayGameplayData = tool(
+  async (_, config: AgentRuntime) => {
+    const { replay_path } = config.context;
+    const replayInfo = await parseOsrFile(replay_path);
+
+    return JSON.stringify(replayInfo.replayData, null, 2);
+  },
+  {
+    name: 'get_replay_gameplay_data',
+    description: 'Retrieve gameplay data (timestamps, mouse coordinates, key presses)',
+  }
+);
 
 export const agent = createAgent({
   model: 'claude-sonnet-4-5-20250929',
   systemPrompt: systemPrompt,
-  tools: [getUserLocation, getWeather],
-  responseFormat,
+  tools: [getReplayInfo, getReplayGameplayData]
 });
